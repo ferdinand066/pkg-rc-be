a277134e6cfd5d09eebfaa783ec40766ca512ccb
ferdinandg066.bo <ferdinandg066@gmail.com>
2025-01-14 18:44:12 +0700

    feat: update borrowing room agreement logic

--- app/Http/Services/BorrowedRoomAgreementService.php
+++ app/Http/Services/BorrowedRoomAgreementService.php
@@ -33,9 +33,24 @@ class BorrowedRoomAgreementService
             return "declined";
         }
 
+        $requiredEmail = [
+            'pkg@parokipik.or.id'
+        ];
+
+        $users = User::whereIn('email', $requiredEmail)->pluck('id')->toArray();
+
+        $countRequiredUsers = BorrowedRoomAgreement::where([
+            ['borrowed_room_id', $borrowedRoom->id], 
+            ['agreement_status', 1]
+        ])->whereIn('created_by_user_id', $users)->count();
+
+        if ($countRequiredUsers !== count($requiredEmail)) {
+            return "pending";
+        }
+
         $count = BorrowedRoomAgreement::where([['borrowed_room_id', $borrowedRoom->id], ['agreement_status', 1]])->count();
         // TODO: set total users to accept a request
-        $userCount = User::where('role', 2)->count();
+        // $userCount = User::where('role', 2)->count();
 
         // if ($count == $userCount) return "accepted";
         if ($count === 2) return "accepted";
